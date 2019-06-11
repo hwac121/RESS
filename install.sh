a68b6572af65ae92394f553ad1081747e8e464a5
hwac121 <50178616+hwac121@users.noreply.github.com>
2019-06-11 18:18:15 -0400

    INCOMPLETE - DO NOT INSTALL

--- install.sh
+++ install.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-sudo cp ress.sh /bin/ress
-sudo chmod +x /bin/ress
-sudo chmod +x ress.sh
-echo "Installation complete"
+
+clear
+echo -e "\e[32m ______ __ __ ____   ___  __    __   ___ ____       ____  _      ____  ___________ \e[0m";
+sleep .15
+echo -e "\e[32m|      |  |  |    \ /   \|  |__|  | /  _]    \     |    \| |    /    |/ ___/      |\e[0m";
+sleep .15
+echo -e "\e[32m|      |  |  |  o  )     |  |  |  |/  [_|  D  )    |  o  ) |   |  o  (   \_|      |\e[0m";
+sleep .15
+echo -e "\e[32m|_|  |_|_   _|   _/|  O  |  |  |  |    _]    /     |     | |___|     |\__  |_|  |_|\e[0m";
+sleep .15
+echo -e "\e[32m  |  | |     |  |  |     |  \`  '  |   [ |    \     |  O  |     |  _  |/  \ | |  |  \e[0m";
+sleep .15
+echo -e "\e[32m  |  | |  |  |  |  |     |\      /|     |  .  \    |     |     |  |  |\    | |  |  \e[0m";
+sleep .15
+echo -e "\e[32m  |__| |__|__|__|   \___/  \_/\_/ |_____|__|\_|    |_____|_____|__|__| \___| |__|  \e[0m";
+sleep .15
+echo -e "\e[32m                                                                                   \e[0m";
+sleep .15
+echo -e "\e[35mVersion 2.0\e[0m";
+sleep .15
+echo -e "...................................................................................................."
+sleep .15
+echo -e " "
+sudo cp txpow.sh /bin/txpow
+sudo chmod +x /bin/txpow
+sudo chmod +x txpow.sh
+echo -e "Installation complete"
 sleep 0.25
-echo "Just type ress anywhere to begin"
+echo -e " "
+echo -e "Just type txpow anywhere to begin"
